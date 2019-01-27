c53bf59e2c4535ca2992da4014757fd30c2a05d9
Barty <josip.bartulovic3@skole.hr>
2019-01-27 22:22:47 +0100

    Created widget panel

--- desktop/UdpClient.java
+++ desktop/UdpClient.java
@@ -1,30 +1,30 @@
-import java.awt.AWTException; 
+import java.awt.AWTException;
 import java.awt.Robot;
 import java.util.concurrent.TimeUnit;
-import java.io.*; 
+import java.io.*;
 import java.net.*;
+import javax.swing.FocusManager;
 
 public class UdpClient {
     public static void main(String[] args) throws IOException, AWTException, InterruptedException {
         System.out.println("TEST");
         Robot rob = new Robot();
         // for(int i=20; i<200; i++){
-        //     rob.mouseMove(i,20);
-        //     System.out.println(i);
-        //     TimeUnit.MILLISECONDS.sleep(100);
+
+        // System.out.println(i);
+        // TimeUnit.MILLISECONDS.sleep(100);
         // }
-
         int PORT = 5005;
-        DatagramSocket serverSocket = new DatagramSocket(PORT, AF_INET);
+        DatagramSocket serverSocket = new DatagramSocket(PORT);
         byte[] receiveData = new byte[1024];
-        byte[] sendData = new byte[1024];
-
-        while(true) {
+        while (true) {
             DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
             serverSocket.receive(receivePacket);
-            String sentence = new String( receivePacket.getData());
-            System.out.println("RECEIVED: " + sentence);
+            String sentence = new String(receivePacket.getData());
+            String[] parts = sentence.split(" ");
+            Integer x = Integer.parseInt(parts[0]);
+            Integer y = Integer.parseInt(parts[1].substring(0, parts[1].length() - 1019));
+            rob.mouseMove(x, y);
         }
-}
     }
 }
--- raspberry/panels/macros/macros.py
+++ raspberry/panels/macros/macros.py
@@ -1,9 +1,50 @@
 from kivy.lang import Builder
 import os
 
+from kivy.config import Config
 from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.stacklayout import StackLayout
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.button import Button
+
+import json
 
 Builder.load_file(os.path.join(os.path.dirname(__file__), "macros.kv"))
 
+Config.set('graphics', 'resizable', '0')
+
+
 class MacrosOption(BoxLayout):
     pass
+
+
+class Macro(BoxLayout):
+    def __init__(self, button=False, **kwargs):
+        super(Macro, self).__init__(**kwargs)
+        if button:
+            self.add_widget(Button(size_hint=[None, None], text="A"))
+
+
+class ButtonGrid(GridLayout):
+    def __init__(self, **kwargs):
+        super(ButtonGrid, self).__init__(**kwargs)
+        self.cols = 12
+        self.rows = 12
+        self.add_macro_page()
+
+    def add_macro_page(self):
+        with open('./data.json') as data:
+            macros = json.load(data)["macro_pages"][0]["macros"]
+            macros.sort(key=lambda macro: macro["position"][0])
+
+            for rows in range(self.rows):
+                macro_row = list(
+                    filter(
+                        lambda macro: macro["position"][1] == rows,
+                        macros))
+                for cols in range(self.cols):
+                    visible = (len(macro_row) and
+                               macro_row[0]["position"][0] == cols)
+                    self.add_widget(Macro(button=visible))
+                    if visible:
+                        macro_row.pop(0)
