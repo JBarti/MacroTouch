238ca6abbc08e95ed2ac40f65a63f059a70346b8
ToninoK <tonino.kastelan@skole.hr>
2019-02-07 22:52:20 +0100

    Added variable ip address

--- desktop/servers/system_monitor.py
+++ desktop/servers/system_monitor.py
@@ -7,20 +7,21 @@ class MonitorServer(threading.Thread):
     def __init__(self, family, sock_type, ip_address="0.0.0.0", port=5300):
         super(MonitorServer, self).__init__()
         self.address = (ip_address, port)
+        self.rpi_address = None
         self.sock = socket.socket(family, sock_type)
         self.sock.bind(self.address)
-        self.request_type = {
-            "GET_SYSTEM_DATA": self.get_system_data,
-            "GET_DEVICE_NAME": self.get_device_name,
-        }
+        self.request_type = {"GET_SYSTEM_DATA": self.get_system_data}
 
     def run(self):
         while True:
-            data = self.sock.recv(1024)
+            data, addr = self.sock.recvfrom(1024)
+            self.rpi_address = addr
             if data != "":
                 json_data = json.loads(data.decode("ASCII"))
-                system_data = self.request_type[json_data["type"]]()
-                self.send_system_data(system_data)
+                req_type = json_data["type"]
+                data = self.request_type[req_type]()
+                if req_type == "GET_SYSTEM_DATA":
+                    self.send_system_data(data)
 
     def get_system_data(self):
         # HERE COMES THE CLASS FOR GETTING SYSTEM MONITOR DATA
@@ -31,7 +32,4 @@ class MonitorServer(threading.Thread):
     def send_system_data(self, data):
         request = {"type": "SET_SYSTEM_DATA", "payload": data}
         bytes_data = bytes(json.dumps(request), "UTF-8")
-        self.sock.sendto(bytes_data, ("rpi_Address", 5300))
-
-    def get_device_name(self):
-        pass
+        self.sock.sendto(bytes_data, self.rpi_address)
--- raspberry/controllers/system_data_controller.py
+++ raspberry/controllers/system_data_controller.py
@@ -1,6 +1,7 @@
 import socket
 import threading
 import json
+from subprocess import check_output
 
 
 class MonitorController(threading.Thread):
@@ -8,6 +9,7 @@ class MonitorController(threading.Thread):
         super(MonitorController, self).__init__()
 
         self.address = (ip_address, port)
+        self.pc_address = None
         self.sock = socket.socket(family, sock_type)
         self.sock.bind(self.address)
 
@@ -18,6 +20,8 @@ class MonitorController(threading.Thread):
         while True:
             data, addr = self.sock.recvfrom(1024)
 
+            self.pc_address = addr
+
             if data != "":
                 json_data = json.loads(data.decode("ASCII"))
 
@@ -27,9 +31,25 @@ class MonitorController(threading.Thread):
                 self.request_type[req_type](req_data)
 
     def get_system_data(self):
-        request = {type: "GET_SYSTEM_DATA"}
+        request = {"type": "GET_SYSTEM_DATA"}
         bytes_data = bytes(json.dumps(request), "UTF-8")
         self.sock.sendto(bytes_data, ("172.21.3.114", 5300))
 
     def set_system_data(self, payload):
         self.data["system_data"] = payload
+
+    def find_pc_address(self):
+        bytes_ip = check_output("hostname -I")
+        local_rpi_ip = bytes_ip.decode("ASCII")[:-1]
+        local_ip = ".".join(local_rpi_ip.split(".")[:-1]) + ".*"
+        ip_string = check_output(["nmap", "-sP", local_ip])
+        ips = " ".join(ip_string.split("\n")).split(" ")
+        ips = [word for word in ips if local_ip in word]
+
+        request = {"type": "SET_RPI_ADDRESS", "rpi_address": local_rpi_ip}
+
+        bytes_data = bytes(json.dumps(request), "UTF-8")
+
+        for ip in ips:
+            self.sock.sendto(bytes_data, (ip, 5300))
+
