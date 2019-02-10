31776a4e8dd36ea6b18f6f0df7eb13dbd226b744
ToninoK <tonino.kastelan@skole.hr>
2019-02-10 02:27:29 +0100

    Optimized the connecting of the devices

---
+++ desktop/app.py
@@ -0,0 +1,11 @@
+from servers import ConnectionServer, MonitorServer, MacroServer
+import socket
+
+conn_server = ConnectionServer(socket.AF_INET, socket.SOCK_DGRAM)
+rpi_host = conn_server.start()
+
+monitor_server = MonitorServer(socket.AF_INET, socket.SOCK_DGRAM, rpi_host)
+macro_server = MacroServer(socket.AF_INET, socket.SOCK_DGRAM)
+
+monitor_server.start()
+macro_server.start()
