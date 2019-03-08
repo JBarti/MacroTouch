36cd5ccc9c113cfe3dfc593442942f532e4c4eb9
ToninoK <tonino.kastelan@skole.hr>
2019-03-08 23:59:03 +0100

    Now returns PC user name

--- desktop/servers/connection.py
+++ desktop/servers/connection.py
@@ -1,5 +1,6 @@
 import socket
 import json
+from subprocess import check_output
 
 
 class ConnectionServer:
@@ -7,10 +8,11 @@ class ConnectionServer:
     def __init__(self, family, sock_type, ip_address="0.0.0.0", port=5010):
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         self.sock.bind((ip_address, port))
+        self.user_name = bytes.decode(check_output("hostname"))[:-1]
 
     def start(self):
         _, addr = self.sock.recvfrom(1024)
         self.sock.sendto(
-            bytes(json.dumps({"type": "PC_ADDRESS"}), "UTF-8"), addr
+            bytes(json.dumps({"type": "PC_ADDRESS", "name":self.user_name}), "UTF-8"), addr
         )
         return addr[0]
