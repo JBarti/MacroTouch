cafefffa29e2f246594df55f0ce10da9e408d3f6
ToninoK <tonino.kastelan@skole.hr>
2019-03-08 23:58:25 +0100

    Adapted for storing multiplle pc hosts to storage

--- raspberry/controllers/connection_controller.py
+++ raspberry/controllers/connection_controller.py
@@ -11,17 +11,21 @@ class ConnectionController:
         self.sock.bind((ip_address, port))
 
     def start(self):
+
+        self.clear_all_hosts()
+
         thread = Thread(target=self.find_pc_address)
         thread.start()
-        _, addr = self.sock.recvfrom(1024)
 
-        with open("./data.json", "r") as jsonFile:
-            data = json.load(jsonFile)
-        print(addr[0])
-        data["pc_host"] = addr[0]
+        while True:
+            _, addr = self.sock.recvfrom(1024)
+            
+            self.add_host(addr)
 
-        with open("./data.json", "w") as jsonFile:
-            json.dump(data, jsonFile)
+            if self.thread_is_done(thread):
+                break
+
+            
 
     def find_pc_address(self):
         """
@@ -31,13 +35,8 @@ class ConnectionController:
         kako biomogućili daljnju komunikaciju
 
         """
-        sleep(1)
-        bytes_ip = check_output(["hostname", "-I"])
-        local_rpi_ip = bytes_ip.decode("ASCII")[:-1].strip()
-        local_ip = ".".join(local_rpi_ip.split(".")[:-1])
-        ip_string = check_output(["nmap", "-sL", local_ip + ".*"]).decode("ASCII")
-        ips = " ".join(ip_string.split("\n")).split(" ")
-        ips = [word for word in ips if local_ip in word]
+        ips, local_rpi_ip = self.find_addresses()
+
         request = {"type": "SET_RPI_ADDRESS", "rpi_address": local_rpi_ip}
 
         bytes_data = bytes(json.dumps(request), "UTF-8")
@@ -48,3 +47,37 @@ class ConnectionController:
             if ip == local_rpi_ip or ip[-3:] == "255" or ip[-2:] == ".0":
                 continue
             self.sock.sendto(bytes_data, (ip, 5010))
+        
+    def find_addresses(self):
+        sleep(1)
+        bytes_ip = check_output(["hostname", "-I"])
+        local_rpi_ip = bytes_ip.decode("ASCII")[:-1].strip()
+        local_ip = ".".join(local_rpi_ip.split(".")[:-1])
+        ip_string = check_output(["nmap", "-sL", local_ip + ".*"]).decode("ASCII")
+        ips = " ".join(ip_string.split("\n")).split(" ")
+        ips = [word for word in ips if local_ip in word]
+        return ips, local_rpi_ip
+
+    def clear_all_hosts(self):
+        with open("./data.json", "r") as jsonFile:
+            data = json.load(jsonFile)
+        
+        data["all_hosts"] = []
+
+        with open("./data.json", "w") as jsonFile:
+            json.dump(data, jsonFile)
+
+    def add_host(self, addr):
+        with open("./data.json", "r") as jsonFile:
+                data = json.load(jsonFile)
+
+        data["all_hosts"].append(addr[0])
+
+        with open("./data.json", "w") as jsonFile:
+            json.dump(data, jsonFile)
+
+    def thread_is_done(self, thread):
+        if not thread.isAlive:
+            sleep(2)
+            return True
+        return False
