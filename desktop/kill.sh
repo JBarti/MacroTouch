045e8f09de2c57a702606879200cbd2d51f56858
ToninoK <tonino9kas@gmail.com>
2019-03-16 22:27:51 +0100

    Made scripts for starting and killing the servers

---
+++ desktop/kill.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+pids=`cat PID.txt`
+
+echo "" > PID.txt
+
+for word in $pids
+do
+    kill 9 $word
+done
+
--- desktop/start.sh
+++ desktop/start.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 python3 ./app.py &
 PYPID=$!
-echo $PYPID
-java UdpClient 
-kill 9 $PYPID
+java UdpClient &
+JPID=$!
+echo "$PYPID  $JPID" >> PID.txt
+
