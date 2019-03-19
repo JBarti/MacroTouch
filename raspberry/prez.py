0f9248c93f90e34ae90d12f0f70edfd02ff00f4b
Barty <josip.bartulovic3@skole.hr>
2019-03-19 15:32:59 +0100

    Sve radi

--- raspberry/prez.py
+++
@@ -1,8 +0,0 @@
-from kivy.uix.button import Button
-from controllers.macro_controller import MacroController
-
-
-class MakroBotun(Button):
-    def on_press(self):
-        controller = MacroController()
-        controller.send_data("<ALT+TAB>")
